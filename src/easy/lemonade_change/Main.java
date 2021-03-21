9675ed29b3c5a6f7227fb9ea554b9c0728dcecb3
Code-Alien <danylo.hreshchuk@gmail.com>
2021-03-21 23:59:06 +0200

    #860. Lemonade Change-fix

--- src/easy/lemonade_change/Main.java
+++ src/easy/lemonade_change/Main.java
@@ -1,2 +1,10 @@
-package easy.lemonade_change;public class Main {
+package easy.lemonade_change;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lemonadeChange(new int[]{5, 5, 10}));
+        System.out.println(solution.lemonadeChange(new int[]{5, 5, 10, 20}));
+        System.out.println(solution.lemonadeChange(new int[]{5, 5, 5, 10, 10, 10, 20, 20}));
+    }
 }
--- src/easy/lemonade_change/Solution.java
+++ src/easy/lemonade_change/Solution.java
@@ -1,2 +1,36 @@
-package easy.lemonade_change;public class Solution {
+package easy.lemonade_change;
+//At a lemonade stand, each lemonade costs $5.
+//Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+//Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.  You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+class Solution {
+//    Runtime: 1 ms, faster than 100.00% of Java online submissions for Lemonade Change.
+//    Memory Usage: 40 MB, less than 64.15% of Java online submissions for Lemonade Change.
+    public boolean lemonadeChange(int[] bills) {
+        int fivesChange = 0;
+        int tensChange = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                fivesChange++;
+            } else if (bill == 10) {
+                if (fivesChange >= 1) {
+                    fivesChange--;
+                    tensChange++;
+                } else {
+                    return false;
+                }
+            } else  if (bill == 20) {
+                if (fivesChange >=1 && tensChange >= 1) {
+                    fivesChange--;
+                    tensChange--;
+                } else if (fivesChange >=3) {
+                    fivesChange -=3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
